1c43cc1bd9638dd24547d64abb68cf4171658987
Ava-S <ava.swevels@gmail.com>
2024-08-13 12:23:57 +0200

    Add queries - merge similar states - remove key properties from event nodes - create relations Update queries - connect event nodes to objects (only create state when there are actually properties)

--- promg_ocel/queries/oced_import.py
+++ promg_ocel/queries/oced_import.py
@@ -47,59 +47,96 @@ class OcedImportQueryLibrary:
                      })
 
     @staticmethod
-    def get_connect_event_nodes_to_objects_with_properties_query(column_id, keys, labels=None):
+    def get_remove_key_properties_from_event_nodes_query(keys):
         query_str = '''
            CALL apoc.periodic.iterate('
                MATCH (e:Event)
-               WHERE e[$id] is not null
-               RETURN e
+               RETURN e, apoc.map.submap(properties(e), $keys, null, false) as properties
            ',
-               'MERGE (n:Entity:$label {sysId: e[$id]})
-                WITH n, e, apoc.map.submap(properties(e), $keys, null, false) as properties
-                CALL apoc.create.removeProperties(e, $keys) YIELD node AS e_removed
-                CALL apoc.create.node(["State"], properties) YIELD node as st
-                MERGE (e_removed) - [:CORR] -> (n)
-                MERGE (n) - [:HAS_STATE] -> (st)
+               'CALL apoc.create.removeProperties(e, $keys) YIELD node AS e_removed
+                RETURN null
                 ',
-               {batchSize: 10000, params: {id: $id, keys:$keys}})            
+               {batchSize: 10000, params: {keys:$keys}})            
+           '''
+
+        return Query(query_str=query_str,
+                     parameters={
+                         "keys": keys,
+                     })
+
+    @staticmethod
+    def get_merge_similar_states_query(keys):
+        query_str = '''
+           CALL apoc.periodic.iterate('
+               MATCH (n:Entity)- [:HAS_STATE] -> (st:State)
+               WITH n, [x in $keys| st[x]] as same_keys, collect(st) as states
+               WHERE size(states) > 1
+               RETURN n
+           ',
+               'MATCH (n) - [:HAS_STATE] -> (st:State)
+               WITH n, [x in $keys| st[x]] as same_keys, collect(st) as states
+               CALL apoc.refactor.mergeNodes(states, {properties: "combine", mergeRels: true})
+               YIELD node
+               RETURN null
+                ',
+               {batchSize: 10000, params: {keys:$keys}})            
+           '''
+
+        return Query(query_str=query_str,
+                     parameters={
+                         "keys": keys,
+                     })
+
+    @staticmethod
+    def get_create_relations_query(_from, _to, _relation):
+        query_str = '''
+           CALL apoc.periodic.iterate('
+               MATCH (_from:$from_labels) <- [:CORR] - (e:Event) - [:CORR] -> (_to:$to_labels)
+               RETURN _from, _to
+           ',
+               'MERGE (_from) -  [:$rel_type] -> (_to)
+                ',
+               {batchSize: 10000})            
            '''
 
         return Query(query_str=query_str,
                      template_string_parameters={
-                         "label": labels[0]
-                     },
-                     parameters={
-                         "id": column_id,
-                         "keys": keys,
-                         "labels": labels if labels is not None else []
+                         "from_labels": ":".join(_from),
+                         "to_labels": ":".join(_to),
+                         "rel_type": _relation
                      })
 
     @staticmethod
-    def get_connect_event_nodes_to_objects_without_properties_query(column_id, labels=None):
+    def get_connect_event_nodes_to_objects_query(column_ids, labels=None, keys=None):
         query_str = '''
-           CALL apoc.periodic.iterate('
-               MATCH (e:Event)
-               WHERE e[$id] is not null
-               RETURN e
-           ',
-               'MERGE (n:Entity {sysId: e[$id]})
-                WITH n, e
-                CALL apoc.create.addLabels(n, $labels) YIELD node AS n_labeled
-                MERGE (e) - [:CORR] -> (n_labeled)
-                ',
-               {batchSize: 10000, params: {id: $id, keys:$keys, labels: $labels}})            
-           '''
+                   CALL apoc.periodic.iterate('
+                       MATCH (e:Event)
+                       WHERE all (x in $ids WHERE e[x] is not null) // check if properties exist
+                       RETURN e
+                   ',
+                       'MERGE (n:Entity $labels {sysId: apoc.text.join([x in $ids| e[x]],"-")})
+                        WITH n, e, apoc.map.submap(properties(e), $keys, null, false) as properties
+                        CALL apoc.do.when(size(keys(properties)) > 1 AND any(x in keys(properties) WHERE properties[
+                        x] is not null),
+                            "CREATE (state:State)
+                            SET state += properties
+                            CREATE (n) - [:HAS_STATE] -> (state)
+                            RETURN n",
+                            "MATCH (n)
+                            RETURN n",
+                            {n:n, properties:properties}
+                            ) YIELD value
+                        WITH e, value.n as n
+                        MERGE (e) - [:CORR] -> (n)
+                        ',
+                       {batchSize: 10000, params: {ids: $ids, keys:$keys}})            
+                   '''
 
         return Query(query_str=query_str,
+                     template_string_parameters={
+                         "labels": ":" + ":".join(labels) if labels is not None else ""
+                     },
                      parameters={
-                         "id": column_id,
-                         "labels": labels if labels is not None else []
+                         "ids": column_ids,
+                         "keys": keys if keys is not None else []
                      })
-
-    @staticmethod
-    def get_connect_event_nodes_to_objects_query(column_id, labels=None, keys=None):
-        if keys is not None:
-            return OcedImportQueryLibrary.get_connect_event_nodes_to_objects_with_properties_query(column_id, keys,
-                                                                                                   labels)
-        else:
-            return OcedImportQueryLibrary.get_connect_event_nodes_to_objects_without_properties_query(column_id, labels)
